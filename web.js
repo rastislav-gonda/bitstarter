dea4389e064959c3a70a21664e82cfb7c4a628d6
Rastislav Gonda <rastislav.gonda@gmail.com>
2013-07-26 17:24:35 +0000

    web,js - read index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var buffer = new Buffer(1024);
+	buffer = fs.readFileSync('index.html');
+	response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
